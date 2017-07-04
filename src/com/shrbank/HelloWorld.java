07df40774152be600e006b06b3041e078de49535
lilei <lilei@shrbank.com>
2017-07-04 19:15:27 +0800

    Hello World

---
+++ src/com/shrbank/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.shrbank;
+
+/**
+ * Created by lilei on 2017/6/10.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!");
+    }
+}
