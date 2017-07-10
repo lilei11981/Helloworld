5dcc311ef80a1d052460eeecfe6770448f8b22bb
lilei <lilei@shrbank.com>
2017-07-10 14:22:46 +0800

    增加FileReadOnly类

---
+++ src/com/shrbank/fileoperate/FileReadOnly.java
@@ -0,0 +1,14 @@
+package com.shrbank.fileoperate;
+
+import java.io.File;
+
+/**
+ * Created by lilei on 2017/7/10.
+ */
+public class FileReadOnly {
+    public static void main(String[] args) {
+        File file = new File("java.txt");
+        System.out.println(file.setReadOnly());
+        System.out.println(file.canWrite());
+    }
+}
