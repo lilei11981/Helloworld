fb16c2e2b6700b0a0fc08e8dbf63edb80cd50673
lilei <lilei@shrbank.com>
2017-07-10 16:28:29 +0800

    增加IsDirectory类以及注释

---
+++ src/com/shrbank/FileContents/IsDirectory.java
@@ -0,0 +1,19 @@
+package com.shrbank.FileContents;
+
+import java.io.File;
+
+/**
+ * Created by lilei on 2017/7/10.
+ * 判断文件夹是否为空
+ */
+public class IsDirectory {
+    public static void main(String[] args) {
+        File file = new File("/Users/lilei/IdeaProjects/HelloWorld");
+        if (file.isDirectory()) {
+            String[] files = file.list();
+            if (files.length > 0) {
+                System.out.println("目录 " + file.getPath() + " 不为空！");
+            }
+        }
+    }
+}
