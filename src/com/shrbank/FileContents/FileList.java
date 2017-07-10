6bee53d3193f493c081debd68c1579f4da118203
lilei <lilei@shrbank.com>
2017-07-10 21:12:18 +0800

    增加FileList类的注释

--- src/com/shrbank/FileContents/FileList.java
+++ src/com/shrbank/FileContents/FileList.java
@@ -9,20 +9,20 @@ import java.io.FileFilter;
  */
 public class FileList {
     public static void main(String[] args) {
-        File dir = new File("/Users/lilei/IdeaProjects/HelloWorld");
-        File[] files = dir.listFiles();
-        FileFilter fileFilter = new FileFilter() {
-            @Override
-            public boolean accept(File file) {
+        File dir = new File("/Users/lilei/IdeaProjects/HelloWorld");   // 实例化File对象
+        File[] files = dir.listFiles();    // 声明一个数组变量files，返回抽象路径名的定义中表示此抽象路径名的目录中的文件的数组
+        FileFilter fileFilter = new FileFilter() {   // 实例化FileFilter对象
+            @Override    // 重写函数
+            public boolean accept(File file) {      //  声明accept()方法，返回值为boolean类型
                 return file.isDirectory();
             }
         };
-        files = dir.listFiles(fileFilter);
+        files = dir.listFiles(fileFilter);    //  fileFilter数组值赋给files
         System.out.println(files.length);
-        if (files.length == 0) {
+        if (files.length == 0) {          //  判断files的长度
             System.out.println("目录不存在或它不是一个目录");
         } else {
-            for (int i = 0; i < files.length; i++) {
+            for (int i = 0; i < files.length; i++) {   // 若长度不为0，则依次输出
                 File fileName = files[i];
                 System.out.println(fileName.toString());
             }
