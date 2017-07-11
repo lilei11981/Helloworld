45f5f47b77e345a1b3c2e43a8523938b08d4984a
lilei <lilei@shrbank.com>
2017-07-11 14:30:57 +0800

    增加ArrayToCollection和GetIP类

---
+++ src/com/shrbank/collection/ArrayToCollection.java
@@ -0,0 +1,26 @@
+package com.shrbank.collection;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by lilei on 2017/7/11.
+ * 使用Array.asList(name)方法将数组转换为集合
+ */
+public class ArrayToCollection {
+    public static void main(String[] args) throws IOException{
+        int n = 5;
+        String[] name = new String[n];    // 声明一个字符串类型的数组变量name，长度为n
+        for (int i = 0; i < n; i++) {
+            name[i] = String.valueOf(i);   // 给数组的每一项赋值
+        }
+
+        List<String> list = Arrays.asList(name);    // 实例化List对象，将数组转换为集合
+//        System.out.println();
+        for (String li : list) {          // 输出集合中的每一项
+            String str = li;
+            System.out.print(str + " ");
+        }
+    }
+}
---
+++ src/com/shrbank/network/GetIP.java
@@ -0,0 +1,21 @@
+package com.shrbank.network;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by lilei on 2017/7/11.
+ * 使用InetAddress类的InetAddress.getByName()方法来获取指定主机的IP地址
+ */
+public class GetIP {
+    public static void main(String[] args) {
+        InetAddress address = null;    // 实例化对象
+        try {
+            address = InetAddress.getByName("lilei1998.com");  // 获取指定主机的IP地址
+        } catch (UnknownHostException e) {
+            System.exit(1);
+        }
+        System.out.println(address.getHostName() + " = " + address.getHostAddress());  // 打印输出主机地址
+        System.exit(0);
+    }
+}
