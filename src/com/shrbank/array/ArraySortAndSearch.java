0a170f247ca3140018a1a97900fe3af1bb8fb2c2
lilei <lilei@shrbank.com>
2017-07-11 11:27:37 +0800

    增加ArraySortAndSearch和StringCompare类

---
+++ src/com/shrbank/array/ArraySortAndSearch.java
@@ -0,0 +1,27 @@
+package com.shrbank.array;
+
+import java.util.Arrays;
+
+/**
+ * Created by lilei on 2017/7/11.
+ */
+public class ArraySortAndSearch {
+    public static void main(String[] args) throws Exception {
+        int[] array = {2, 5, -2, 6, -3, 8, 0, -7, -9, 4};
+        Arrays.sort(array);
+        printArray("数组排序结果为", array);
+        int index = Arrays.binarySearch(array, 4);
+        System.out.println("元素 4 在第 " + (index + 1) + " 个位置！");
+    }
+
+    private static void printArray(String message, int array[]) {
+        System.out.println(message + ": [length: " + array.length + "]");
+        for (int i = 0; i < array.length; i++) {
+            if ( i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+}
