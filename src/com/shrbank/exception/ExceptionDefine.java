b92ec589ee8667b728393c30b7bb0e43c6076655
lilei <lilei@shrbank.com>
2017-07-12 17:50:01 +0800

    增加ExceptionDefine类和InToPost类

---
+++ src/com/shrbank/exception/ExceptionDefine.java
@@ -0,0 +1,27 @@
+package com.shrbank.exception;
+
+/**
+ * Created by lilei on 2017/7/12.
+ * 通过继承Exception类来实现自定义异常
+ */
+class WrongInputException extends Exception {  // 自定义的类
+    WrongInputException(String s) {
+        super(s);
+    }
+}
+
+class Input {
+    void method() throws WrongInputException {
+        throw new WrongInputException("Wrong input");    // 抛出自定义的类
+    }
+}
+
+public class ExceptionDefine {
+    public static void main(String[] args) {
+        try {
+            new Input().method();
+        } catch (WrongInputException wie) {
+            System.out.println(wie.getMessage());
+        }
+    }
+}
