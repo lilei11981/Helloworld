b6fd493e20fd60f32624cf7af0bc728986ef5f52
lilei <lilei@shrbank.com>
2017-07-13 17:34:53 +0800

    增加CollectionSize类和CollectionShuffle类

---
+++ src/com/shrbank/collection/CollectionShuffle.java
@@ -0,0 +1,25 @@
+package com.shrbank.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by lilei on 2017/7/13.
+ */
+public class CollectionShuffle {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Integer(i));
+        }
+        System.out.println("打乱前：");
+        System.out.println(list);
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println("第 " + i + " 次打乱：");
+            Collections.shuffle(list);
+            System.out.println(list);
+        }
+    }
+}
