bd6de55230ee00ea805dd03bee9f11bfe2af2107
lilei <lilei@shrbank.com>
2017-07-13 15:32:34 +0800

    增加VectorMaxElement类

---
+++ src/com/shrbank/datastructure/VectorMaxElement.java
@@ -0,0 +1,20 @@
+package com.shrbank.datastructure;
+
+import java.util.Collections;
+import java.util.Vector;
+
+/**
+ * Created by lilei on 2017/7/13.
+ * 使用Vector类的v.add()方法和Collection类的Collections.max()方法来获取向量的最大值
+ */
+public class VectorMaxElement {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        v.add(new Double(3.14));
+        v.add(new Double(2.7));
+        v.add(new Double(1.414));
+        v.add(new Double(1.73));
+        Object obj = Collections.max(v);
+        System.out.println("最大元素是：" + obj);
+    }
+}
