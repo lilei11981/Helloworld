1ca370f4c92e59fe3e36d38262d7fedae54b3b67
lilei <lilei@shrbank.com>
2017-07-13 16:58:58 +0800

    增加CollectionTraversal类

---
+++ src/com/shrbank/collection/CollectionTraversal.java
@@ -0,0 +1,23 @@
+package com.shrbank.collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * Created by lilei on 2017/7/13.
+ * 使用Collection类的iterator()方法来遍历集合
+ */
+public class CollectionTraversal {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("1", "1st");
+        hashMap.put("2", "2nd");
+        hashMap.put("3", "3rd");
+        Collection collection = hashMap.values();      // 把hashMap值放入collection集合中
+        Iterator iterator = collection.iterator();     // 使用iterator()方法来遍历集合
+        while (iterator.hasNext()) {                   // 如果下一项存在，则输出下一项
+            System.out.println(iterator.next());
+        }
+    }
+}
