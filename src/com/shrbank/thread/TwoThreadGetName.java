9893bd038633e1efca11e1cfa403bcf66b7c1125
lilei <lilei@shrbank.com>
2017-07-16 11:17:27 +0800

    修改线程类变量名

--- src/com/shrbank/thread/TwoThreadGetName.java
+++ src/com/shrbank/thread/TwoThreadGetName.java
@@ -12,16 +12,16 @@ public class TwoThreadGetName extends Thread{
     }
 
     public void printMsg() {
-        Thread t = Thread.currentThread();
-        String name = t.getName();
+        Thread thread = Thread.currentThread();
+        String name = thread.getName();
         System.out.println("name = " + name);
     }
 
     public static void main(String[] args) {
-        TwoThreadGetName tt = new TwoThreadGetName();
-        tt.start();
+        TwoThreadGetName twoThreadGetName = new TwoThreadGetName();
+        twoThreadGetName.start();
         for (int i = 0; i < 10; i++) {
-            tt.printMsg();
+            twoThreadGetName.printMsg();
         }
     }
 }
