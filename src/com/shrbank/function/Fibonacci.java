38c81c02e885ceea3ea7135a4a49bb74b1d7ed4b
lilei11981 <1198189741@qq.com>
2017-07-21 09:31:11 +0800

    新增Fibonacci类，修改配置文件

---
+++ src/com/shrbank/function/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.shrbank.function;
+
+/**
+ * Created by lilei on 2017/7/20 下午6:16.
+ * 输出斐波那契数列
+ * 从第三项开始，每一项等于前两项之和
+ */
+public class Fibonacci {
+    private static long fibonacci(long number) {
+        if ((number == 0) || (number == 1)) {
+            return number;
+        } else {
+            return fibonacci(number - 1) + fibonacci(number - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 34; i++) {
+            System.out.printf("Fibonacci of %d is: %d\n", i, fibonacci(i));
+        }
+    }
+}
