ff713308d49d8dbd0954e17439f18c116d9d7a88
lilei <lilei@shrbank.com>
2017-07-25 19:25:17 +0800

    修改LengthOf2DArray类

--- src/com/shrbank/array/LengthOf2DArray.java
+++ src/com/shrbank/array/LengthOf2DArray.java
@@ -2,7 +2,7 @@ package com.shrbank.array;
 
 /**
  * Created by lilei on 2017/7/25 下午5:50.
- * 使用数组的属性length来获取数组的长度
+ * 使用数组的length属性来获取数组的长度
  */
 public class LengthOf2DArray {
     public static void main(String[] args) {
