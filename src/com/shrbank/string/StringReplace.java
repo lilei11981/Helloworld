16a27f34be8e9819f79a266ea58988bba10bc078
lilei <lilei@shrbank.com>
2017-08-12 13:11:47 +0800

    新增StringReplace类

--- src/com/shrbank/string/StringReplace.java
+++ src/com/shrbank/string/StringReplace.java
@@ -2,6 +2,17 @@ package com.shrbank.string;
 
 /**
  * Created by lilei on 2017/7/12.
+ * 使用String类的replace()方法来替换字符串中的字符
  */
 public class StringReplace {
+    public static void main(String[] args) {
+        String str = "Hello Sansa!";
+        System.out.println(str);
+
+        System.out.println(str.replace('H','W'));
+        System.out.println(str.replace("He", "Wa"));
+        System.out.println(str.replace("He", "Ha"));
+
+        System.out.println(str.replace("Sansa", "Jon"));
+    }
 }
