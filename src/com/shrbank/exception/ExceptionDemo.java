d3315f88fe71b55b66102a84f25870d1c54bb0bb
lilei11981 <1198189741@qq.com>
2017-09-04 17:54:12 +0800

    配置文件修改

--- src/com/shrbank/exception/ExceptionDemo.java
+++ src/com/shrbank/exception/ExceptionDemo.java
@@ -5,15 +5,15 @@ package com.shrbank.exception;
  */
 public class ExceptionDemo {
     public static void main(String[] args) {
-//        try {
-//            throw new Exception("My Exception");
-//        } catch (Exception e) {
-//            System.err.println("Caught Exception");
-//            System.err.println("getMessage():" + e.getMessage());
-//            System.err.println("getLocalizedMessage():" + e.getLocalizedMessage());
-//            System.err.println("toString():" + e);
-//            System.err.println("printStackTrace():");
-//            e.printStackTrace();
-//        }
+        try {
+            throw new Exception("My Exception");
+        } catch (Exception e) {
+            System.err.println("Caught Exception");
+            System.err.println("getMessage():" + e.getMessage());
+            System.err.println("getLocalizedMessage():" + e.getLocalizedMessage());
+            System.err.println("toString():" + e);
+            System.err.println("printStackTrace():");
+            e.printStackTrace();
+        }
     }
 }
