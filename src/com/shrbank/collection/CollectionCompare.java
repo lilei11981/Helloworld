eab90f845f1e6da44205731d2f5a8d8553fdce44
lilei <lilei@shrbank.com>
2017-09-05 16:07:32 +0800

    修改CollectionCompare类

--- src/com/shrbank/collection/CollectionCompare.java
+++ src/com/shrbank/collection/CollectionCompare.java
@@ -11,10 +11,8 @@ import java.util.TreeSet;
 public class CollectionCompare {
     public static void main(String[] args) {
         String[] coins = {"Penny", "nickel", "dime", "Quarter", "dollar"};
-        Set set = new TreeSet();
-        for (int i = 0; i < coins.length; i++) {
-            set.add(coins[i]);
-        }
+        Set<String> set = new TreeSet<>();
+        Collections.addAll(set, coins);
         System.out.println(Collections.min(set));        //  不忽略大小写，首字母ASCII值最小为P，最大为n； 忽略大小写，最小为d，最大为Q
         System.out.println(Collections.min(set, String.CASE_INSENSITIVE_ORDER));
         for (int i = 0; i <= 10; i++) {
