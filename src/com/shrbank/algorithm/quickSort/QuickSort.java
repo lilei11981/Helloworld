004d3b1f7be8592eb7d3806da6f1daa6b36acbdc
lilei <lilei@shrbank.com>
2017-09-14 10:00:16 +0800

    修改QuickSort类，快速排序法实现

--- src/com/shrbank/algorithm/quickSort/QuickSort.java
+++ src/com/shrbank/algorithm/quickSort/QuickSort.java
@@ -3,6 +3,7 @@ package com.shrbank.algorithm.quickSort;
 /**
  * Created by lilei on 2017/9/7 下午3:09.
  */
+
 public class QuickSort{
     public static void main(String []args){
         System.out.println("快速排序法");
@@ -16,8 +17,8 @@ public class QuickSort{
         int end = a.length-1;
         sort(a,start,end);
         System.out.println("排序后：");
-        for(int i = 0; i<a.length; i++){
-            System.out.print(a[i]+ " ");
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
         }
     }
     private static void sort(int[] a, int low, int high){
@@ -25,19 +26,19 @@ public class QuickSort{
         int end = high;
         int key = a[low];
 
-        while(end>start){
+        while(end > start){
             //从后往前比较
-            while(end>start&&a[end]>=key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
+            while(end > start && a[end] >= key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
                 end--;
-            if(a[end]<=key){
+            if(a[end] <= key){
                 int temp = a[end];
                 a[end] = a[start];
                 a[start] = temp;
             }
             //从前往后比较
-            while(end>start&&a[start]<=key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
+            while(end > start && a[start] <= key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
                 start++;
-            if(a[start]>=key){
+            if(a[start] >= key){
                 int temp = a[start];
                 a[start] = a[end];
                 a[end] = temp;
@@ -45,7 +46,7 @@ public class QuickSort{
             //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
         }
         //递归
-        if(start>low) sort(a,low,start-1);//左边序列。第一个索引位置到关键值索引-1
-        if(end<high) sort(a,end+1,high);//右边序列。从关键值索引+1到最后一个
+        if(start > low) sort(a, low, start-1);//左边序列。第一个索引位置到关键值索引-1
+        if(end < high) sort(a, end+1, high);//右边序列。从关键值索引+1到最后一个
     }
 }
