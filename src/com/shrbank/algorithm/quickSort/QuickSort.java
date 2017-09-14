1d85154594b340a95a903b833b7be51b9cf208b9
lilei <lilei@shrbank.com>
2017-09-14 09:56:11 +0800

    新增QuickSort类，快速排序法实现

--- src/com/shrbank/algorithm/quickSort/QuickSort.java
+++ src/com/shrbank/algorithm/quickSort/QuickSort.java
@@ -3,32 +3,49 @@ package com.shrbank.algorithm.quickSort;
 /**
  * Created by lilei on 2017/9/7 下午3:09.
  */
-public class QuickSort {
-
-    public static void main(String[] args) {
-        Quick quick = new Quick();
-        quick.sort(new int[]{5,4,5,6,2,5,6,7,2},5, 2);
-    }
-
-}
-
-class Quick {
-    public static int sort(int[] arr, int low, int high) {
-        int key = arr[low];
-        while (low < high) {
-            while (low < high && arr[high] >= key) {
-                high--;
-            }
-            arr[low] = arr[high];
-
-            while (low < high && arr[low] <= key) {
-                low++;
-            }
-            arr[high] = arr[low];
+public class QuickSort{
+    public static void main(String []args){
+        System.out.println("快速排序法");
+        int[] a = {12,20,5,16,15,1,30,45,23,9};
+        System.out.println("排序前：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
         }
-        arr[high] = key;
-        return high;
+        System.out.println();
+        int start = 0;
+        int end = a.length-1;
+        sort(a,start,end);
+        System.out.println("排序后：");
+        for(int i = 0; i<a.length; i++){
+            System.out.print(a[i]+ " ");
+        }
     }
+    private static void sort(int[] a, int low, int high){
+        int start = low;
+        int end = high;
+        int key = a[low];
 
-    public static void quickSort()
+        while(end>start){
+            //从后往前比较
+            while(end>start&&a[end]>=key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
+                end--;
+            if(a[end]<=key){
+                int temp = a[end];
+                a[end] = a[start];
+                a[start] = temp;
+            }
+            //从前往后比较
+            while(end>start&&a[start]<=key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
+                start++;
+            if(a[start]>=key){
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+            }
+            //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
+        }
+        //递归
+        if(start>low) sort(a,low,start-1);//左边序列。第一个索引位置到关键值索引-1
+        if(end<high) sort(a,end+1,high);//右边序列。从关键值索引+1到最后一个
+    }
 }
