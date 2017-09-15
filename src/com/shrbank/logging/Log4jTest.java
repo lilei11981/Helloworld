a8e153b42e1a18b725b110cf30f0bbb7383a68a8
lilei <lilei@shrbank.com>
2017-09-15 14:11:04 +0800

    修改log4j.properties配置文件

--- src/com/shrbank/logging/Log4jTest.java
+++ src/com/shrbank/logging/Log4jTest.java
@@ -6,14 +6,13 @@ import org.apache.log4j.Logger;
  * Created by lilei on 2017/9/15 上午10:43.
  */
 public class Log4jTest {
-    public static Logger logger1 = Logger.getLogger(Log4jTest.class);
+    public static Logger logger = Logger.getLogger(Log4jTest.class);
     public static void main(String[] args) {
-        //logger1
-        logger1.trace("我是logger1，trace");
-        logger1.debug("我是logger1，debug");
-        logger1.info("我是logger1，info");
-        logger1.warn("我是logger1，warn");
-        logger1.error("我是logger1，error");
-        logger1.fatal("我是logger1，fatal");
+        logger.trace("我是logger，trace");
+        logger.debug("我是logger，debug");
+        logger.info("我是logger，info");
+        logger.warn("我是logger，warn");
+        logger.error("我是logger，error");
+        logger.fatal("我是logger，fatal");
     }
 }
