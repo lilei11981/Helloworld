edf128149f723fa6cdbfad46edf88908ab03d4bf
李磊 <1198189741@qq.com>
2017-10-23 15:38:55 +0800

    新增十六进制转二进制方法

--- src/com/shrbank/util/AES.java
+++ src/com/shrbank/util/AES.java
@@ -100,11 +100,31 @@ public class AES {
         return sb.toString();
     }
 
+    /**
+     * 将16进制转换为二进制
+     *
+     * @param hexStr 十六进制数
+     * @return 二进制数
+     */
+    public static byte[] parseHexStr2Byte(String hexStr) {
+        if (hexStr.length() < 1)
+            return null;
+        byte[] result = new byte[hexStr.length() / 2];
+        for (int i = 0; i < hexStr.length() / 2; i++) {
+            int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+            int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2),
+                    16);
+            result[i] = (byte) (high * 16 + low);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(AES.encrypt("123", "456")));
-        byte[] array = new byte[]{-45, -111, 38, -73, -118, -70, 42, 119, -9, -48, 107, 51, 19, 57, -51, -31};
-        System.out.println(Arrays.toString(AES.decrypt(array, "456")));
-        System.out.println(AES.parseByte2HexStr(array));
+//        System.out.println(Arrays.toString(AES.encrypt("123", "456")));
+//        byte[] array = new byte[]{10, 10};
+//        System.out.println(Arrays.toString(AES.decrypt(array, "456")));
+//        System.out.println(AES.parseByte2HexStr(array));
+//        System.out.println(Arrays.toString(AES.parseHexStr2Byte("12")));
     }
 
 }
