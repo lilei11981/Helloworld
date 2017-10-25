f02c77df3d7607d78c911a39608775dda4ea5a9c
李磊 <1198189741@qq.com>
2017-10-25 09:32:58 +0800

    新增DigitUtil类

---
+++ src/com/shrbank/util/DigitUtil.java
@@ -0,0 +1,9 @@
+package com.shrbank.util;
+
+/**
+ * Created by lilei on 2017/10/24 下午4:01.
+ */
+public class DigitUtil {
+    private static int[] _crossLine = new int[] {9, 99, 999, 9999, 99999, 999999, 9999999, 99999999};
+    private static String[] _comp = new String[] { "", "0", "00", "000", "0000", "00000", "000000", "0000000", "00000000" };
+}
