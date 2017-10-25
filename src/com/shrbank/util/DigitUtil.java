916cc360e2fc8ccc49baab464e01bc0226afa8d0
李磊 <1198189741@qq.com>
2017-10-25 09:50:49 +0800

    规范代码提交

--- src/com/shrbank/util/DigitUtil.java
+++ src/com/shrbank/util/DigitUtil.java
@@ -6,4 +6,28 @@ package com.shrbank.util;
 public class DigitUtil {
     private static int[] _crossLine = new int[] {9, 99, 999, 9999, 99999, 999999, 9999999, 99999999};
     private static String[] _comp = new String[] { "", "0", "00", "000", "0000", "00000", "000000", "0000000", "00000000" };
+
+    /**
+     * 左补零，补齐长度为指定位数的数字，并转换成字节数组返回
+     *
+     * @param digit 位数
+     * @param maxDigit 最大位数
+     * @return 字节数组
+     * @throws IndexOutOfBoundsException 数组越界异常
+     */
+    public static byte[] digit2Bytes(int digit, int maxDigit) {
+        String s = "";
+        int max = maxDigit - 1;  // 数组
+        for (int i = 0; i < maxDigit; i++) {  // 遍历
+            if (digit <= _crossLine[i]) {
+                s = _comp[max - i];
+                break;
+            }
+            if (i == maxDigit - 1) {
+                throw new IndexOutOfBoundsException("The max digit is "
+                        + _comp.length + ", digit-->bytes convert failed!");
+            }
+        }
+        return (s + digit).getBytes();
+    }
 }
