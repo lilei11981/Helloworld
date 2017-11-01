dc4d8b80539e7b788c4b5667abf92b8f4f995673
李磊 <1198189741@qq.com>
2017-11-01 17:37:45 +0800

    配置文件修改

---
+++ src/com/shrbank/test/Foo.java
@@ -0,0 +1,11 @@
+package com.shrbank.test;
+
+/**
+ * Created by lilei on 2017/11/1 下午3:29.
+ */
+public class Foo {
+    public static void main(String[] args) {
+        String s = "s";
+        System.out.println("s = " + s);
+    }
+}
