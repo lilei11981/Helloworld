117aa4cf8effe23a3870f0aacbd9fa9d3b30fc50
李磊 <1198189741@qq.com>
2017-11-02 10:35:19 +0800

    String和StringBuffer对比

--- src/com/shrbank/test/Foo.java
+++ src/com/shrbank/test/Foo.java
@@ -7,5 +7,13 @@ public class Foo {
     public static void main(String[] args) {
         String s = "s";
         System.out.println("s = " + s);
+        String string = "HELLO WORLD!";
+        System.out.println(string.length());
+        StringBuffer stringBuffer = new StringBuffer("hello world!");
+        System.out.println(stringBuffer.reverse());
+        StringBuffer reverse = stringBuffer.reverse();
+        StringBuffer reverseTwice = reverse.reverse();
+        System.out.println(reverseTwice);
+
     }
 }
