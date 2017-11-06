f5d9271962b3b9db0729839da5f37a678557af29
李磊 <1198189741@qq.com>
2017-11-06 14:10:02 +0800

    新增测试类

---
+++ src/com/shrbank/test/ConditionTest.java
@@ -0,0 +1,22 @@
+package com.shrbank.test;
+
+/**
+ * Created by lilei on 2017/11/6 上午11:10.
+ */
+public class ConditionTest {
+    public static void main(String[] args) {
+        Object o1 = true ? new Integer(1) : new Double(2.0);
+        Object o2;
+        if (true) {
+            o2 = new Integer(1);
+        } else {
+            o2 = new Double(2.0);
+        }
+        System.out.println(o1);
+        System.out.println(o2);
+    }
+
+    /*
+    * 三目运算符会在有需要的情况下，对操作数进行类型提升
+    * */
+}
