1ac670746f07c65260e7966df671f92508473d01
李磊 <1198189741@qq.com>
2017-11-16 18:16:51 +0800

    使用延迟执行线程池的方法

--- src/com/shrbank/test/ExecutorsTest.java
+++ src/com/shrbank/test/ExecutorsTest.java
@@ -2,6 +2,8 @@ package com.shrbank.test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by lilei on 2017/11/16 下午5:32.
@@ -18,7 +20,10 @@ public class ExecutorsTest {
 //        ExecutorService pool = Executors.newSingleThreadExecutor();
 
         // 可变尺寸线程池
-        ExecutorService pool = Executors.newCachedThreadPool();
+//        ExecutorService pool = Executors.newCachedThreadPool();
+
+        // 创建一个线程池，它可安排在给定延迟后运行命令或者定期地执行。
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(2);
 
         // 创建线程
         Thread t1 = new MyThread();
@@ -28,8 +33,13 @@ public class ExecutorsTest {
         Thread t5 = new MyThread();
         // 将线程放入池中进行执行
         pool.execute(t1);
-        pool.execute(t2);
-        pool.execute(t3);
+//        pool.execute(t2);
+//        pool.execute(t3);
+
+        // 使用延迟执行风格的方法
+        pool.schedule(t2, 1000, TimeUnit.MILLISECONDS);
+        pool.schedule(t3, 10, TimeUnit.MILLISECONDS);
+
         pool.execute(t4);
         pool.execute(t5);
         // 关闭线程池
