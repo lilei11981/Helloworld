c316f364b8046c952723fcf01175f76130d9308a
李磊 <1198189741@qq.com>
2017-11-17 10:49:35 +0800

    代码格式修改

--- src/com/shrbank/thread/ThreadPoolTest.java
+++ src/com/shrbank/thread/ThreadPoolTest.java
@@ -11,22 +11,22 @@ import java.util.concurrent.TimeUnit;
 public class ThreadPoolTest implements Runnable {
     public void run() {
         synchronized(this) {
-            try{
+            try {
                 System.out.println(Thread.currentThread().getName());
                 Thread.sleep(3000);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e){
                 e.printStackTrace();
             }
         }
     }
 
     public static void main(String[] args) {
-        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
+        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
         ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 6, 1, TimeUnit.DAYS, queue);
         for (int i = 0; i < 10; i++) {
-            executor.execute(new Thread(new ThreadPoolTest(), "TestThread".concat(""+i)));
+            executor.execute(new Thread(new ThreadPoolTest(), "TestThread".concat("" + i)));
             int threadSize = queue.size();
-            System.out.println("线程队列大小为-->"+threadSize);
+            System.out.println("线程队列大小为-->" + threadSize);
         }
         executor.shutdown();
     }
