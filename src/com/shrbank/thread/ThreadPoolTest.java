bc3d94bbdbccce37e47efbca59bac165dab41beb
李磊 <1198189741@qq.com>
2017-11-17 15:21:32 +0800

    LinkedBlockingQueue<Runnable>队列使用

--- src/com/shrbank/thread/ThreadPoolTest.java
+++ src/com/shrbank/thread/ThreadPoolTest.java
@@ -1,9 +1,6 @@
 package com.shrbank.thread;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by lilei on 2017/11/17 上午10:32.
@@ -12,16 +9,17 @@ public class ThreadPoolTest implements Runnable {
     public void run() {
         synchronized(this) {
             try {
-                System.out.println(Thread.currentThread().getName());
-                Thread.sleep(3000);
+                System.out.println("线程名称：" + Thread.currentThread().getName());
+                Thread.sleep(3000); // 休眠是为了让线程不至于执行完毕后从线程池释放
             } catch (InterruptedException e){
                 e.printStackTrace();
             }
         }
     }
 
-    public static void main(String[] args) {
-        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+    public static void main(String[] args) throws InterruptedException {
+//        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(4);  // 固定为4的线程队列
         ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 6, 1, TimeUnit.DAYS, queue);
         for (int i = 0; i < 10; i++) {
             executor.execute(new Thread(new ThreadPoolTest(), "TestThread".concat("" + i)));
