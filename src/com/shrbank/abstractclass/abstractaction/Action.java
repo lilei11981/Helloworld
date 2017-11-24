fec5a9f63ab368f6c485821179f65de6c4b4b3cd
李磊 <1198189741@qq.com>
2017-11-24 22:42:51 +0800

    演示抽象类定义模板的功能

--- src/com/shrbank/abstractclass/abstractaction/Action.java
+++ src/com/shrbank/abstractclass/abstractaction/Action.java
@@ -7,4 +7,33 @@ public abstract class Action {
 
     public static final int EAT = 1;
     public static final int SLEEP = 2;
+    public static final int WORK = 5;
+
+    public abstract void eat();
+    public abstract void sleep();
+    public abstract void work();
+
+    public void command(int flags) {
+        switch (flags) {
+            case EAT:
+                this.eat();
+                break;
+            case SLEEP:
+                this.sleep();
+                break;
+            case WORK:
+                this.work();
+                break;
+            case EAT + SLEEP:
+                this.eat();
+                this.sleep();
+                break;
+            case SLEEP + WORK:
+                this.sleep();
+                this.work();
+                break;
+            default:
+                break;
+        }
+    }
 }
---
+++ src/com/shrbank/abstractclass/abstractaction/Human.java
@@ -0,0 +1,23 @@
+package com.shrbank.abstractclass.abstractaction;
+
+/**
+ * Created by lilei on 2017/11/24 下午10:34.
+ *
+ * 定义人的类，扩展 Action 抽象类
+ */
+public class Human extends Action {
+    @Override
+    public void eat() {
+        System.out.println("人吃饭");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("人睡觉");
+    }
+
+    @Override
+    public void work() {
+        System.out.println("人工作");
+    }
+}
