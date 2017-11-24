87582094869a3d4088edb8a6c32e9d93dd8f0ace
李磊 <1198189741@qq.com>
2017-11-24 17:48:58 +0800

    HelloWorld代码修改

--- src/com/shrbank/HelloWorld.java
+++ src/com/shrbank/HelloWorld.java
@@ -6,6 +6,6 @@ package com.shrbank;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.printf("HELLO WORLD!");
     }
 }
