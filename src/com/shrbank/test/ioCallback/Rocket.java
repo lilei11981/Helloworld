c6254f33fb5ec7f6463f7352de23d0749e879ecb
李磊 <1198189741@qq.com>
2017-12-01 18:16:21 +0800

    测试接口回调过程

---
+++ src/com/shrbank/test/ioCallback/Rocket.java
@@ -0,0 +1,40 @@
+package com.shrbank.test.ioCallback;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lilei
+ * Date: 2017-12-01
+ * Time: 下午5:51
+ * Description:
+ * 火箭类，使用到回调接口的方法
+ */
+
+public class Rocket {
+
+    // 火箭将及时的操作委托给委托者处理
+    private Delegate delegate;
+
+    public Rocket(Delegate delegate) {
+        super();
+        this.delegate = delegate;
+    }
+
+    // 获取开始发送时间
+    public long getRocketStartTime() {
+        return delegate.startTime();
+    }
+
+    // 获取发送结束时间
+    public long getRocketEndTime() {
+        return delegate.endTime();
+    }
+
+    // 判断是否发送成功
+    public boolean isSendSuccessful() {
+        if (getRocketStartTime() - getRocketEndTime() <= 1000) {
+            return true;
+        }
+        delegate.sendFailedInfo();
+        return false;
+    }
+}
---
+++ src/com/shrbank/test/ioCallback/Test.java
@@ -0,0 +1,17 @@
+package com.shrbank.test.ioCallback;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lilei
+ * Date: 2017-12-01
+ * Time: 下午6:12
+ * Description:
+ * 测试让专家来发射火箭
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        Expert expert = new Expert();
+        expert.send();
+    }
+}
