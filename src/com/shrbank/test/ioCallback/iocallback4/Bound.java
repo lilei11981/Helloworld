c589c80c86d2160038ba11159370646cfe66a592
李磊 <1198189741@qq.com>
2017-12-04 16:56:31 +0800

    新增接口回调测试类

---
+++ src/com/shrbank/test/ioCallback/iocallback4/Bound.java
@@ -0,0 +1,14 @@
+package com.shrbank.test.ioCallback.iocallback4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lilei
+ * Date: 2017-12-04
+ * Time: 下午4:46
+ * Description:
+ * 测试接口回调
+ */
+
+public interface Bound {
+    public void doresult(String s);
+}
---
+++ src/com/shrbank/test/ioCallback/iocallback4/You.java
@@ -0,0 +1,19 @@
+package com.shrbank.test.ioCallback.iocallback4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lilei
+ * Date: 2017-12-04
+ * Time: 下午4:51
+ * Description:
+ * 接口回调测试
+ */
+
+public class You {
+    public void dosth(Bound me, String task) {
+        // TODO
+        System.out.println("你要我做" + task);
+        String result = "做完，搞定";
+        me.doresult(result);
+    }
+}
