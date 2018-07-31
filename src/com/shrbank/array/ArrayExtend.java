25d9d437082ebf14bb155ec38c7f26a2c8c75fff
李磊 <1198189741@qq.com>
2018-07-31 17:42:47 +0800

    新增生产环境信息

--- src/com/shrbank/array/ArrayExtend.java
+++ src/com/shrbank/array/ArrayExtend.java
@@ -2,6 +2,11 @@ package com.shrbank.array;
 
 /**
  * Created by lilei on 2017/9/1 下午5:07.
+ * public static void arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+ * 它可以实现将一个数组的指定个数元素复制到另一个数组中
+ * 举例：
+ * arrayCopy(arr1, 2, arr2, 5, 10);
+ * 将arr1数组里从索引为2的元素开始, 复制到数组arr2里的索引为5的位置, 复制的元素个数为10个.
  */
 public class ArrayExtend {
     public static void main(String[] args) {
@@ -11,7 +16,7 @@ public class ArrayExtend {
         extended[4] = "E";
         System.arraycopy(names, 0, extended, 0, names.length);
         for (String string : extended){
-            System.out.println(string);
+            System.out.print(string + " ");
         }
     }
 }
