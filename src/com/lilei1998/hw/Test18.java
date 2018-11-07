98ecffe2788756e5193a45716ae607e14f79db71
李磊 <1198189741@qq.com>
2018-11-07 17:24:38 +0800

    优化压缩重复小写字母的类

--- src/com/lilei1998/hw/Test18.java
+++ src/com/lilei1998/hw/Test18.java
@@ -18,9 +18,8 @@ import java.util.Scanner;
  * 1、仅压缩连续重复出现的字符。比如字符串"abcbc"由于无连续重复字符，压缩后的字符串还是"abcbc"。
  * 2、压缩字段的格式为"字符重复的次数+字符"。例如：字符串"xxxyyyyyyz"压缩后就成为"3x6yz"。
  * 要求实现函数：
- * void stringZip(const char *pInputStr, long lInputLen, char *pOutputStr);
- * 输入pInputStr：  输入字符串lInputLen：  输入字符串长度
- * 输出 pOutputStr： 输出字符串，空间已经开辟好，与输入字符串等长；
+ * void stringZip(String inputString);
+ * inputString：输入字符串
  * 注意：只需要完成该函数功能算法，中间不需要有任何IO的输入输出
  * 示例
  * 输入：“cccddecc”   输出：“3c2de2c”
@@ -58,9 +57,9 @@ public class Test18 {
                 } else {
                     break;
                 }
-                if (count[i] != 1) {
-                    stringBuffer.append(count[i]);
-                }
+            }
+            if (count[i] != 1) {
+                stringBuffer.append(count[i]);
             }
             stringBuffer.append(list.get(i));
             System.out.println("count[" + i + "]" + count[i]);
