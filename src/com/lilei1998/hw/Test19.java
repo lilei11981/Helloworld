0e42c36bc85c03b9c983b11d4f67ab45ea40bd72
李磊 <1198189741@qq.com>
2018-11-09 10:48:43 +0800

    新增测试类

---
+++ src/com/lilei1998/hw/Test19.java
@@ -0,0 +1,16 @@
+package com.lilei1998.hw;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lilei
+ * Date: 2018-11-08
+ * Time: 5:34 PM
+ * Description:
+ * 将一个十进制（byte型）转换为二进制，讲二进制数前后颠倒，再算出颠倒后对于的十进制数。
+ */
+
+public class Test19 {
+    public static void main(String[] args) {
+
+    }
+}
