6538f75062e89fbb2167132b31dcd5a836bf8bec
李磊 <1198189741@qq.com>
2018-11-09 17:09:21 +0800

    优化十进制转二进制反转后转十进制的类

--- src/com/lilei1998/hw/Test19.java
+++ src/com/lilei1998/hw/Test19.java
@@ -11,6 +11,16 @@ package com.lilei1998.hw;
 
 public class Test19 {
     public static void main(String[] args) {
-
+        int number = 10;
+        String binary = Integer.toBinaryString(number);
+        System.out.println("二进制为：" + binary);
+        char[] chars = binary.toCharArray();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            stringBuffer.append(chars[i]);
+        }
+        System.out.println("二进制颠倒后：" + stringBuffer.toString());
+        Integer integer = Integer.valueOf(stringBuffer.toString(), 2);
+        System.out.println("对应的十进制：" + integer);
     }
 }
