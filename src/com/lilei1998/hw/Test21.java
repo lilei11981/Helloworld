94b1adeb8513a84f4db95ff3ee252c6685f8891c
李磊 <1198189741@qq.com>
2018-11-14 14:33:32 +0800

    代码更新

--- src/com/lilei1998/hw/Test21.java
+++ src/com/lilei1998/hw/Test21.java
@@ -1,15 +1,27 @@
 package com.lilei1998.hw;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: lilei
  * Date: 2018-11-09
  * Time: 6:06 PM
  * Description:
+ * 找出输出字符串里面所有大写字母，将其逆序输出
  */
 
 public class Test21 {
     public static void main(String[] args) {
+        Test21 test = new Test21();
+        Scanner input = new Scanner(System.in);
+        String string = input.next();
+        String result = test.stringReverse(string);
+        System.out.println(result);
+    }
 
+    public String stringReverse(String input) {
+
+        return null;
     }
 }
