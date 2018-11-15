99fd5884636ec6599de12bd3b3a6003d09c382fb
李磊 <1198189741@qq.com>
2018-11-15 14:17:16 +0800

    代码修改，修改代码结构

--- src/com/lilei1998/hw/Test22.java
+++ src/com/lilei1998/hw/Test22.java
@@ -11,9 +11,8 @@ import java.util.Scanner;
  * 通过键盘输入任意一个字符串序列，字符串可能包含多个子串，子串以空格分割。
  * 请编写一个程序，自动分离出各个子串，并使用','将其分离，并且在最后也补充一个','，并将子串存储。
  * 要求实现函数：
- * 【输入】   pInputStr       输入字符串
- * IInputLen       输入字符串长度
- * 【输出】   pOutputStr      输出字符串，空间已经开劈好，与输入输入字符串等长；
+ * 【输入】   inputString       输入字符串
+ * 【输出】   newString         输出字符串，空间已经开劈好，与输入输入字符串等长；
  * 【注意】   只需要完成该函数功能算法，中间不需要有任何IO的输入输出
  * 示例：
  * 输入："abc def ghi       d"
@@ -25,20 +24,18 @@ public class Test22 {
         Test22 test = new Test22();
         Scanner input = new Scanner(System.in);
         String string = input.nextLine();
-        long length = string.length();
-        String newString = "";
-        String result = test.divideString(string, length, newString);
+        String result = test.divideString(string);
         System.out.println(result);
     }
 
-    public String divideString(String inputStr, long len, String newStr) {
-        String newInputString = inputStr.trim();
+    public String divideString(String inputString) {
+        String newInputString = inputString.trim();
         System.out.println("原字符串：" + newInputString);
-        newStr = inputStr.replaceAll("\\s+", ",");
-        System.out.println("新字符串：" + newStr);
-        if (newStr.charAt(newStr.length() - 1) != ',') {
-            newStr += ',';
+        String newString = inputString.replaceAll("\\s+", ",");
+        System.out.println("新字符串：" + newString);
+        if (newString.charAt(newString.length() - 1) != ',') {
+            newString += ',';
         }
-        return newStr;
+        return newString;
     }
 }
