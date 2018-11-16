92e35767f0674a84160611ecf34cb1122f5ad47b
李磊 <1198189741@qq.com>
2018-11-16 15:32:32 +0800

    新增测试类

---
+++ src/com/lilei1998/hw/Test25.java
@@ -0,0 +1,14 @@
+package com.lilei1998.hw;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lilei
+ * Date: 2018-11-16
+ * Time: 3:26 PM
+ * Description:
+ * 输入一段字符串，字符串中包含空格、数字、符号和字母（包含大小写），要求完成函数使得输出给定字符串中最长字符串，输出的字符串可以包含数字和字符，不可以有空格和符号。若有相同长度的字符串，则输出第一个。
+ * 例如：输入:hello huawei!，输出为:huawei
+ */
+
+public class Test25 {
+}
