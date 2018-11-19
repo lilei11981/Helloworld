f740c98645d545d282e30d34e5697d7b0822fb09
李磊 <1198189741@qq.com>
2018-11-19 14:41:36 +0800

    新增测试类 Java8新特性 函数式接口 LambdaExpressions

--- src/com/lilei1998/java8/LambdaExpression.java
+++ src/com/lilei1998/java8/LambdaExpression.java
@@ -1,10 +1,5 @@
 package com.lilei1998.java8;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 /**
  * Created with IntelliJ IDEA.
  * User: lilei
@@ -16,43 +11,48 @@ import java.util.List;
 
 public class LambdaExpression {
     public static void main(String[] args) {
-        List<String> names1 = new ArrayList<String>();
-        names1.add("Google ");
-        names1.add("Runoob ");
-        names1.add("Taobao ");
-        names1.add("Baidu ");
-        names1.add("Sina ");
+        LambdaExpression tester = new LambdaExpression();
 
-        List<String> names2 = new ArrayList<String>();
-        names2.add("Google ");
-        names2.add("Runoob ");
-        names2.add("Taobao ");
-        names2.add("Baidu ");
-        names2.add("Sina ");
+        // 类型声明
+        MathOperation addition = (int a, int b) -> a + b;
 
-        LambdaExpression test = new LambdaExpression();
+        // 不用类型声明
+        MathOperation subtraction = (a, b) -> a - b;
 
-        System.out.println("使用 Java 7 语法: ");
-        test.sortUsingJava7(names1);
-        System.out.println(names1);
+        // 大括号中的返回语句
+        MathOperation multiplication = (int a, int b) -> {
+            return a * b;
+        };
 
-        System.out.println("使用 Java 8 语法: ");
-        test.sortUsingJava8(names2);
-        System.out.println(names2);
+        // 没有大括号及返回语句
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        // 不用括号
+        GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        // 用括号
+        GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+        greetService1.sayMessage("Facebook");
+        greetService2.sayMessage("Google");
     }
 
-    // 使用 java 7 排序
-    private void sortUsingJava7(List<String> names) {
-        Collections.sort(names, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return s1.compareTo(s2);
-            }
-        });
+    private int operate(int a, int b, MathOperation mathOperation) {
+        return mathOperation.operation(a, b);
     }
 
-    // 使用 java 8 排序
-    private void sortUsingJava8(List<String> names) {
-        Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
     }
 }
