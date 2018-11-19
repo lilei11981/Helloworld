2a4637e5fdf1d125ed0af0936d36c6e1e735a6f9
李磊 <1198189741@qq.com>
2018-11-19 14:12:14 +0800

    新增测试类 Java8新特性 方法引用 MethodReference

---
+++ src/com/lilei1998/java8/MethodReferences.java
@@ -0,0 +1,25 @@
+package com.lilei1998.java8;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lilei
+ * Date: 2018-11-19
+ * Time: 2:00 PM
+ * Description:
+ * Java8新特性 方法引用
+ */
+
+public class MethodReferences {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Google");
+        names.add("Runoob");
+        names.add("Taobao");
+        names.add("Baidu");
+        names.add("Sina");
+
+        names.forEach(System.out::println);
+    }
+}
