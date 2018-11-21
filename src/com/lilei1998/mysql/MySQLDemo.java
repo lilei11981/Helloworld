a7a45b12d926f7b6777327f4373c33029370f39d
李磊 <1198189741@qq.com>
2018-11-21 15:38:58 +0800

    新增mysql测试类 新增jar包 修改配置文件

--- src/com/lilei1998/mysql/MySQLDemo.java
+++ src/com/lilei1998/mysql/MySQLDemo.java
@@ -21,7 +21,7 @@ public class MySQLDemo {
 
     // JDBC 驱动名及数据库 URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/lilei";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/lilei";
 
     // 数据库的用户名与密码，需要根据自己的设置
     static final String USER = "root";
