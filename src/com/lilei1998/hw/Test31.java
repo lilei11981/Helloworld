e89943b20dd2fe13ed103e7a97cfe74bbb8a621e
李磊 <1198189741@qq.com>
2018-12-13 15:32:03 +0800

    在给定字符串中查找所有特定子串并删除，如果没有找到相应子串，则不作任何操作，变量名修改，注释修改

--- src/com/lilei1998/hw/Test31.java
+++ src/com/lilei1998/hw/Test31.java
@@ -10,9 +10,9 @@ import java.util.Scanner;
  * Description:
  * 在给定字符串中查找所有特定子串并删除，如果没有找到相应子串，则不作任何操作。
  * 要求实现函数：
- * int getNumber(String firstString, String secondString)
- * 【输入】 firstString：输入的被操作字符串 secondString：需要查找并删除的特定子字符串
- * 【输出】 firstString：在firstString字符串中删除所有secondString子字符串后的结果
+ * int getNumber(String originString, String subString)
+ * 【输入】 originString：输入的被操作字符串 subString：需要查找并删除的特定子字符串
+ * 【输出】 resultString：在originString字符串中删除所有subString子字符串后的结果
  * 【返回】 删除的子字符串的个数
  * 注：
  * I、 子串匹配只考虑最左匹配情况，即只需要从左到右进行字串匹配的情况。比如：
@@ -20,11 +20,11 @@ import java.util.Scanner;
  * 匹配出从左到右位置2开始的"aba"，则不是最左匹配，且只能匹配出1个"aba"字串。
  * II、 输入字符串不会超过100 Bytes，请不用考虑超长字符串的情况。
  * 示例
- * 输入：firstString = "abcde123abcd123"  secondString = "123"
- * 输出：firstString = "abcdeabcd"
+ * 输入：originString = "abcde123abcd123"  subString = "123"
+ * 输出：resultString = "abcdeabcd"
  * 返回：2
- * 输入：firstString = "abcde123abcd123*****fdaklfa11123*123jfdlafl"  secondString = "1234"
- * 输出：firstString = "abcde123abcd123*****fdaklfa11123*123jfdlafl"
+ * 输入：originString = "abcde123abcd123*****fdaklfa11123*123jfdlafl"  subString = "1234"
+ * 输出：resultString = "abcde123abcd123*****fdaklfa11123*123jfdlafl"
  * 返回：0
  */
 
@@ -37,18 +37,20 @@ public class Test31 {
         System.out.println("删除子串的个数：" + result);
     }
 
-    public static int getNumber(String firstString, String secondString) {
-        System.out.println("原字符串：" + firstString);
-        System.out.println("子串：" + secondString);
+    public static int getNumber(String originString, String subString) {
+        System.out.println("原字符串：" + originString);
+        System.out.println("子串：" + subString);
         int count = 0;
-        while (firstString.contains(secondString)) {
-            firstString = firstString.substring(0, firstString.indexOf(secondString))
-                    .concat(firstString.substring(firstString.indexOf(secondString)
-                            + secondString.length()));
+        String resultString = "";
+        ;
+        while (originString.contains(subString)) {
+            resultString = originString.substring(0, originString.indexOf(subString))
+                    .concat(originString.substring(originString.indexOf(subString)
+                            + subString.length()));
             count++;
             System.out.println("进入循环，count值：" + count);
         }
-        System.out.println("输出的字符串：" + firstString);
+        System.out.println("输出的字符串：" + resultString);
         return count;
     }
 }
