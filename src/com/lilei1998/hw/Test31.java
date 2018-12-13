1a2bba005701d8a806ab8ff661f26706bfc50384
李磊 <1198189741@qq.com>
2018-12-13 14:42:09 +0800

    在给定字符串中查找所有特定子串并删除，如果没有找到相应子串，则不作任何操作，格式修改

--- src/com/lilei1998/hw/Test31.java
+++ src/com/lilei1998/hw/Test31.java
@@ -10,10 +10,9 @@ import java.util.Scanner;
  * Description:
  * 在给定字符串中查找所有特定子串并删除，如果没有找到相应子串，则不作任何操作。
  * 要求实现函数：
- * int delete_sub_str(const char *str, const char *sub_str, char *result_str)
- * 【输入】 str：输入的被操作字符串
- * sub_str：需要查找并删除的特定子字符串
- * 【输出】 result_str：在str字符串中删除所有sub_str子字符串后的结果
+ * int getNumber(String firstString, String secondString)
+ * 【输入】 firstString：输入的被操作字符串 secondString：需要查找并删除的特定子字符串
+ * 【输出】 firstString：在firstString字符串中删除所有secondString子字符串后的结果
  * 【返回】 删除的子字符串的个数
  * 注：
  * I、 子串匹配只考虑最左匹配情况，即只需要从左到右进行字串匹配的情况。比如：
@@ -21,13 +20,11 @@ import java.util.Scanner;
  * 匹配出从左到右位置2开始的"aba"，则不是最左匹配，且只能匹配出1个"aba"字串。
  * II、 输入字符串不会超过100 Bytes，请不用考虑超长字符串的情况。
  * 示例
- * 输入：str = "abcde123abcd123"
- * sub_str = "123"
- * 输出：result_str = "abcdeabcd"
+ * 输入：firstString = "abcde123abcd123"  secondString = "123"
+ * 输出：firstString = "abcdeabcd"
  * 返回：2
- * 输入：str = "abcde123abcd123*****fdaklfa11123*123jfdlafl"
- * sub_str = "1234"
- * 输出：result_str = "abcde123abcd123"
+ * 输入：firstString = "abcde123abcd123*****fdaklfa11123*123jfdlafl"  secondString = "1234"
+ * 输出：firstString = "abcde123abcd123*****fdaklfa11123*123jfdlafl"
  * 返回：0
  */
 
@@ -41,7 +38,8 @@ public class Test31 {
     }
 
     public static int getNumber(String firstString, String secondString) {
-        System.out.println("原字符串：" + firstString + "子串：" + secondString);
+        System.out.println("原字符串：" + firstString);
+        System.out.println("子串：" + secondString);
         int count = 0;
         while (firstString.indexOf(secondString) != -1) {
             firstString = firstString.substring(0, firstString.indexOf(secondString))
