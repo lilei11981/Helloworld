c3714a5dfb19693ff0ae33d4077c766e1d126874
lilei11981 <1198189741@qq.com>
2019-06-09 17:18:47 +0800

    代码更新

--- src/com/company/hw/Test39.java
+++ src/com/company/hw/Test39.java
@@ -24,4 +24,72 @@ public class Test39 {
             }
         }
     }
+
+    // 选择排序
+    public static void selectSort(int[] a) {
+        if (a == null || a.length <= 0) {
+            return;
+        }
+        for (int i = 0; i < a.length; i++) {
+            int temp = a[i];
+            // 将当前下标定义为最小值下标
+            int flag = i;
+            for (int j = i + 1; j < a.length; j++) {
+                // a[j] < temp 从小到大排序；a[j] > temp 从大到小排序
+                if (a[j] < temp) {
+                    temp = a[j];
+                    // 如果有小于当前最小值的关键字将此关键字的下标赋值给flag
+                    flag = j;
+                }
+            }
+            if (flag != i) {
+                a[flag] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+
+    // 快速排序
+    public static void quickSort(int[] a) {
+        if (a.length > 0) {
+            quickSort(a, 0, a.length - 1);
+        }
+    }
+
+    private static void quickSort(int[] a, int low, int high) {
+        //1,找到递归算法的出口
+        if (low > high) {
+            return;
+        }
+        //2, 存
+        int i = low;
+        int j = high;
+        //3,key
+        int key = a[low];
+        //4，完成一趟排序
+        while (i < j) {
+            //4.1 ，从右往左找到第一个小于key的数
+            while (i < j && a[j] > key) {
+                j--;
+            }
+            // 4.2 从左往右找到第一个大于key的数
+            while (i < j && a[i] <= key) {
+                i++;
+            }
+            //4.3 交换
+            if (i < j) {
+                int p = a[i];
+                a[i] = a[j];
+                a[j] = p;
+            }
+        }
+        // 4.4，调整key的位置
+        int p = a[i];
+        a[i] = a[low];
+        a[low] = p;
+        //5, 对key左边的数快排
+        quickSort(a, low, i - 1);
+        //6, 对key右边的数快排
+        quickSort(a, i + 1, high);
+    }
 }
