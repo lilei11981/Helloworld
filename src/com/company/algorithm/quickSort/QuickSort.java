97a56743794ba195cd439d122423715dbd546314
lilei11981 <1198189741@qq.com>
2019-06-25 12:54:48 +0800

    快速排序代码优化

--- src/com/company/algorithm/quickSort/QuickSort.java
+++ src/com/company/algorithm/quickSort/QuickSort.java
@@ -29,16 +29,18 @@ public class QuickSort {
 
         while (end > start) {
             //从后往前比较
-            while (end > start && a[end] >= key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
-                end--;
+            while (end > start && a[end] >= key) {
+                end--;//如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
+            }
             if (a[end] <= key) {
                 int temp = a[end];
                 a[end] = a[start];
                 a[start] = temp;
             }
             //从前往后比较
-            while (end > start && a[start] <= key)  //如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
-                start++;
+            while (end > start && a[start] <= key) {
+                start++;//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
+            }
             if (a[start] >= key) {
                 int temp = a[start];
                 a[start] = a[end];
@@ -47,7 +49,11 @@ public class QuickSort {
             //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
         }
         //递归
-        if (start > low) sort(a, low, start - 1);   //左边序列。第一个索引位置到关键值索引-1
-        if (end < high) sort(a, end + 1, high);     //右边序列。从关键值索引+1到最后一个
+        if (start > low) {
+            sort(a, low, start - 1); //左边序列。第一个索引位置到关键值索引-1
+        }
+        if (end < high) {
+            sort(a, end + 1, high);     //右边序列。从关键值索引+1到最后一个
+        }
     }
 }
