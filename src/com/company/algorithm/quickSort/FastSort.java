a705a4689c0df28e6e02e41a6729fd804aaccfe0
lilei11981 <1198189741@qq.com>
2019-06-25 12:56:10 +0800

    快速排序代码优化

--- src/com/company/algorithm/quickSort/FastSort.java
+++ src/com/company/algorithm/quickSort/FastSort.java
@@ -17,14 +17,18 @@ public class FastSort {
         j = high;
         index = a[i]; // 用子表的第一个记录做基准
         while (i < j) { // 从表的两端交替向中间扫描
-            while (i < j && a[j] >= index)
+            while (i < j && a[j] >= index) {
                 j--;
-            if (i < j)
+            }
+            if (i < j) {
                 a[i++] = a[j];// 用比基准小的记录替换低位记录
-            while (i < j && a[i] < index)
+            }
+            while (i < j && a[i] < index) {
                 i++;
-            if (i < j) // 用比基准大的记录替换高位记录
-                a[j--] = a[i];
+            }
+            if (i < j) {
+                a[j--] = a[i];// 用比基准大的记录替换高位记录
+            }
         }
         a[i] = index;// 将基准数值替换回 a[i]
         sort(a, low, i - 1); // 对低子表进行递归排序
