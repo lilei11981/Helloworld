e8b08826ba2f123770aed06f3826532c17c55c3c
lilei11981 <1198189741@qq.com>
2020-07-06 11:25:35 +0800

    代码更新

--- src/com/company/mysql/MySQLDemo.java
+++ src/com/company/mysql/MySQLDemo.java
@@ -20,19 +20,19 @@ import java.sql.SQLException;
 public class MySQLDemo {
 
     // JDBC 驱动名及数据库 URL
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/lilei";
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/java?useSSL=false";
 
     // 数据库的用户名与密码，需要根据自己的设置
-    static final String USER = "root";
-    static final String PASS = "123456";
+    private static final String USER = "root";
+    private static final String PASS = "123456";
 
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
         try {
             // 注册 JDBC 驱动
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName(JDBC_DRIVER);
 
             // 打开链接
             System.out.println("连接数据库...");
@@ -42,20 +42,20 @@ public class MySQLDemo {
             System.out.println(" 实例化Statement对象...");
             stmt = (Statement) conn.createStatement();
             String sql;
-            sql = "SELECT id, name, url FROM websites";
+            sql = "SELECT * FROM tb_user";
             ResultSet rs = stmt.executeQuery(sql);
 
             // 展开结果集数据库
             while (rs.next()) {
                 // 通过字段检索
                 int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String url = rs.getString("url");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
 
                 // 输出数据
                 System.out.print("ID: " + id);
-                System.out.print(", 站点名称: " + name);
-                System.out.print(", 站点 URL: " + url);
+                System.out.print(", 用户名 " + username);
+                System.out.print(", 密码 " + password);
                 System.out.print("\n");
             }
             // 完成后关闭
