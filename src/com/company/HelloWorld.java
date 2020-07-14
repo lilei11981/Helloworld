f22d5030d4fda10945925d92ac66d6a89c1b94cc
lilei11981 <1198189741@qq.com>
2020-07-14 13:29:10 +0800

    代码更新

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.sun.corba.se.impl.orbutil.graph.Graph;
-
 /**
  * @author lilei
  * Hello World 输出
@@ -215,28 +213,28 @@ public class HelloWorld {
         定义大小为N的dp数组，对于第一行，dp[i]=dp[i-1]+m[0][i],
         在求第二行中的 dp[i] 时可以覆盖第一行 dp[i] ,第二行dp[i]=Math.min（dp[i],dp[i-1]）+m[i][j]。
     */
-    public static int shortRoad2(int[][] m) {
-        int[] dp = new int[m[0].length];
-        dp[0] = m[0][0];
-        for (int i = 1; i < dp.length; i++) {
-            //求出第一行的dp
-            dp[i] = dp[i - 1] + m[0][i];
-        }
-        for (int i = 1; i < m.length; i++) {
-            //求出每一行的第一个dp[0] 后一行的dp[0]覆盖前一行的dp[0]
-            dp[0] = m[i][0] + dp[0];
-            for (int j = 1; j < dp.length; j++) {
-                dp[j] = Math.min(dp[j - 1], dp[j]) + m[i][j];
-            }
-        }
-        return dp[dp.length - 1];
-    }
-
-    public static void main(String[] args) {
-        int[][] m = {{4, 1, 5, 3}, {3, 2, 7, 7}, {6, 5, 2, 8}, {8, 9, 4, 5}};
-        int[][] n = {{1, 3, 5, 9}, {8, 1, 3, 4}, {5, 0, 6, 1}, {8, 8, 4, 0}};
-        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        System.out.println(shortRoad2(a));
-        Graph graph = null;
-    }
+//    public static int shortRoad2(int[][] m) {
+//        int[] dp = new int[m[0].length];
+//        dp[0] = m[0][0];
+//        for (int i = 1; i < dp.length; i++) {
+//            //求出第一行的dp
+//            dp[i] = dp[i - 1] + m[0][i];
+//        }
+//        for (int i = 1; i < m.length; i++) {
+//            //求出每一行的第一个dp[0] 后一行的dp[0]覆盖前一行的dp[0]
+//            dp[0] = m[i][0] + dp[0];
+//            for (int j = 1; j < dp.length; j++) {
+//                dp[j] = Math.min(dp[j - 1], dp[j]) + m[i][j];
+//            }
+//        }
+//        return dp[dp.length - 1];
+//    }
+//
+//    public static void main(String[] args) {
+//        int[][] m = {{4, 1, 5, 3}, {3, 2, 7, 7}, {6, 5, 2, 8}, {8, 9, 4, 5}};
+//        int[][] n = {{1, 3, 5, 9}, {8, 1, 3, 4}, {5, 0, 6, 1}, {8, 8, 4, 0}};
+//        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//        System.out.println(shortRoad2(a));
+//        Graph graph = null;
+//    }
 }
