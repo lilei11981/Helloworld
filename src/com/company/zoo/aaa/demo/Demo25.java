5ddfddad8b732cdc83cfb9c51973ec7dd6543614
lilei11981 <1198189741@qq.com>
2021-01-26 20:42:10 +0800

    两数之和问题更新

--- src/com/company/zoo/aaa/demo/Demo25.java
+++ src/com/company/zoo/aaa/demo/Demo25.java
@@ -18,16 +18,16 @@ public class Demo25 {
         System.out.println(Arrays.toString(result));
     }
 
-    private static int[] twoSum(int[] nums, int target) {
+    private static int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+        Map<Integer, Integer> map = new HashMap<>(numbers.length);
+        for (int i = 0; i < numbers.length; i++) {
+            map.put(numbers[i], i);
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i]) && !map.get(target - nums[i]).equals(i)) {
-                result[0] = i;
-                result[1] = map.get(target - nums[i]);
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i]) && !map.get(target - numbers[i]).equals(i)) {
+                result[0] = i + 1;
+                result[1] = map.get(target - numbers[i]) + 1;
                 break;
             }
         }
