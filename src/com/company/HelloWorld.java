1763a3d5ae4f480f62ae52d069a7d2ffbdd7b0c5
lilei11981 <1198189741@qq.com>
2021-01-28 15:44:50 +0800

    新增注释

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -1247,18 +1247,217 @@ public class HelloWorld {
 
     /////////////////////////////////////////////////////////
 
-    public static void main(String[] args) {
-        System.out.println(JumpFloor(3));
+//    public static void main(String[] args) {
+//        System.out.println(JumpFloor(3));
+//    }
+//
+//    public static int JumpFloor(int target) {
+//        if (target == 1) {
+//            return 1;
+//        } else if (target == 2) {
+//            return 2;
+//        } else {
+//            return JumpFloor(target - 1) + JumpFloor(target - 2);
+//        }
+//    }
+
+
+//    public class ImplementStackUsingQueues_225 {
+//        private Queue<Integer> queueIn;
+//        private Queue<Integer> queueOut;
+//
+//        /** Initialize your data structure here. */
+//        public ImplementStackUsingQueues_225() {
+//            queueIn = new LinkedBlockingQueue<Integer>();
+//            queueOut = new LinkedBlockingQueue<Integer>();
+//        }
+//
+//        /** Push element x onto stack. */
+//        public void push(int x) {
+//            while(!queueOut.isEmpty()){
+//                queueIn.add(queueOut.poll());
+//            }
+//            queueIn.add(x);
+//        }
+//
+//        /** Removes the element on top of the stack and returns that element. */
+//        public int pop() {
+//            while(!queueOut.isEmpty()){
+//                queueIn.add(queueOut.poll());
+//            }
+//            int length = queueIn.size();
+//            for(int i=0;i<length-1;i++){
+//                queueOut.add(queueIn.poll());
+//            }
+//            return queueIn.poll();
+//        }
+//
+//        /** Get the top element. */
+//        public int top() {
+//            while(!queueOut.isEmpty()){
+//                queueIn.add(queueOut.poll());
+//            }
+//            int length = queueIn.size();
+//            for(int i=0;i<length-1;i++){
+//                queueOut.add(queueIn.poll());
+//            }
+//            int result = queueIn.peek();
+//            queueOut.add(queueIn.poll());
+//            return result;
+//        }
+//
+//        /** Returns whether the stack is empty. */
+//        public boolean empty() {
+//            return queueIn.isEmpty() && queueOut.isEmpty();
+//        }
+//    }
+
+    /*
+    public class ArrayQueue {
+    private String[] datas;
+    private int head;
+    private int tail;
+    private int count;
+
+    public ArrayQueue(int size){
+        datas = new String[size];
+        count = 0;
+        head = 0;
+        tail = 0;
     }
 
-    public static int JumpFloor(int target) {
-        if (target == 1) {
-            return 1;
-        } else if (target == 2) {
-            return 2;
-        } else {
-            return JumpFloor(target - 1) + JumpFloor(target - 2);
+    public boolean enqueue(String data){
+        if(count==0){
+            //空队列
+            datas[head] = data;
+            count++;
+            return true;
+        }else if(count==datas.length){
+            //队列满了
+            return false;
+        }else{
+            datas[++tail%datas.length] = data;
+            count++;
+            return true;
+        }
+
+    }
+
+    public String dequeue(){
+        if(count==0){
+            //空队列
+            return null;
+        }else{
+            String data = datas[head];
+            head = ++head%datas.length;
+            count--;
+            return data;
         }
     }
+
+    public void printQueue(){
+        for(int i=0;i<count;i++){
+            System.out.println(datas[(head+i)%datas.length]);
+        }
+    }
+}
+    * */
+
+    /*
+    public class LinkedListQueue {
+
+    private Node head = null;
+    private Node tail = null;
+    private int size = 5;
+    private int count = 0;
+
+    public boolean enqueue(String data){
+        if(count==size){
+            //队列满了
+            return false;
+        }
+        if(count==0){
+            //空队列
+            Node node = new Node(data,null);
+            head = node;
+            tail = node;
+            count++;
+        }else{
+            tail.next = new Node(data, null);
+            tail = tail.next;
+            count++;
+        }
+        return true;
+    }
+
+    public Node dequeue(){
+        if(count==0){
+            return null;
+        }else{
+            Node node = head;
+            head = head.next;
+            count--;
+            return node;
+        }
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public class Node {
+        public String data;
+        public Node next;
+
+        public Node(String data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
+
+     */
+
+
+//    public class ImplementQueueUsingStack_232 {
+//        private Stack<Integer> stackIn;
+//        private Stack<Integer> stackOut;
+//
+//        /** Initialize your data structure here. */
+//        public ImplementQueueUsingStack_232() {
+//            stackIn = new Stack<Integer>();
+//            stackOut = new Stack<Integer>();
+//        }
+//
+//        /** Push element x to the back of queue. */
+//        public void push(int x) {
+//            while(!stackOut.isEmpty()){
+//                stackIn.push(stackOut.pop());
+//            }
+//
+//            stackIn.push(x);
+//        }
+//
+//        /** Removes the element from in front of queue and returns that element. */
+//        public int pop() {
+//            while(!stackIn.isEmpty()){
+//                stackOut.push(stackIn.pop());
+//            }
+//            return stackOut.pop();
+//        }
+//
+//        /** Get the front element. */
+//        public int peek() {
+//            while(!stackIn.isEmpty()){
+//                stackOut.push(stackIn.pop());
+//            }
+//            return stackOut.peek();
+//        }
+//
+//        /** Returns whether the queue is empty. */
+//        public boolean empty() {
+//            return stackIn.isEmpty() && stackOut.isEmpty();
+//        }
+//    }
 }
 
