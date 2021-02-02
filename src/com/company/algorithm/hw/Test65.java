75a33eed46b2af428d17eb597d733a0afd769d3a
lilei11981 <1198189741@qq.com>
2021-02-02 20:46:12 +0800

    合并有序链表

---
+++ src/com/company/algorithm/hw/Test65.java
@@ -0,0 +1,59 @@
+package com.company.algorithm.hw;
+
+/**
+ * @author lilei
+ * @date 2021-02-02 下午3:45
+ * @apiNote 将两个有序的链表合并为一个新链表，要求新的链表是通过拼接两个链表的节点来生成的，且合并后新链表依然有序。
+ */
+
+public class Test65 {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(5);
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(3);
+        System.out.println(mergeTwoLists(l1, l2));
+    }
+
+    /**
+     * @param l1 ListNode类
+     * @param l2 ListNode类
+     * @return ListNode类
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode result = null;
+        if (l1.val < l2.val) {
+            result = l1;
+            result.next = mergeTwoLists(l1.next, l2);
+        } else {
+            result = l2;
+            result.next = mergeTwoLists(l1, l2.next);
+        }
+        return result;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
