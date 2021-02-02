740ac62a8314f55eb21d77f5da96a07e8ea2d3c8
lilei11981 <1198189741@qq.com>
2021-02-02 14:59:45 +0800

    二叉树先序、中序和后续遍历测试

--- src/com/company/algorithm/hw/Test63.java
+++ src/com/company/algorithm/hw/Test63.java
@@ -29,6 +29,8 @@ public class Test63 {
         for (int[] res : result) {
             System.out.println(Arrays.toString(res));
         }
+
+
     }
 
     /**
@@ -49,7 +51,7 @@ public class Test63 {
         if (root == null) {
             return 0;
         }
-        return getRootSize(root.left) + getRootSize(root.right) + 1;
+        return 1 + getRootSize(root.left) + getRootSize(root.right);
     }
 
     public static void preOrder(TreeNode root, int[][] result) {
@@ -61,26 +63,26 @@ public class Test63 {
         preOrder(root.right, result);
     }
 
-    public static void midOrder(TreeNode root, int[][] res) {
+    public static void midOrder(TreeNode root, int[][] result) {
         if (root == null) {
             return;
         }
-        midOrder(root.left, res);
-        res[1][midIndex++] = root.val;
-        midOrder(root.right, res);
+        midOrder(root.left, result);
+        result[1][midIndex++] = root.val;
+        midOrder(root.right, result);
     }
 
-    public static void postOrder(TreeNode root, int[][] res) {
+    public static void postOrder(TreeNode root, int[][] result) {
         if (root == null) {
             return;
         }
-        postOrder(root.left, res);
-        postOrder(root.right, res);
-        res[2][postIndex++] = root.val;
+        postOrder(root.left, result);
+        postOrder(root.right, result);
+        result[2][postIndex++] = root.val;
     }
 
     public static class TreeNode {
-        int val = 0;
+        int val;
         TreeNode left = null;
         TreeNode right = null;
 
