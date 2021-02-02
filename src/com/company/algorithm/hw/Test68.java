d7f65532a377f8467de081ca354a8fe657360405
lilei <919874206@qq.com>
2021-02-02 23:30:11 +0800

    求两个字符串的最长公共子串

--- src/com/company/algorithm/hw/Test68.java
+++ src/com/company/algorithm/hw/Test68.java
@@ -13,7 +13,7 @@ public class Test68 {
      * @param str2 string字符串 the string
      * @return string字符串
      */
-    public String LCS(String str1, String str2) {
+    public static String LCS(String str1, String str2) {
         // write code here
         if (str1 == null || str2 == null) {
             return "-1";
@@ -22,7 +22,7 @@ public class Test68 {
         StringBuilder stringBuilder = new StringBuilder();
         int start = 0;
         int end = 0;
-        while (end < str1.length() + 1) {
+        while (end < str1.length()) {
             if (str2.contains(str1.substring(start, end))) {
                 if (stringBuilder.length() < end - start) {
                     stringBuilder.delete(0, stringBuilder.length());
@@ -37,5 +37,9 @@ public class Test68 {
             return "-1";
         }
         return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LCS("1AB2345CD","1234EF"));
     }
 }
