2c7033cc5c6492de2632c5e20b7653b23226c7e5
lilei11981 <1198189741@qq.com>
2021-02-07 17:26:27 +0800

    验证SimpleDateFormat类的线程不安全，使用ThreadLocal解决

---
+++ src/com/company/zoo/aaa/demo/Demo33.java
@@ -0,0 +1,44 @@
+package com.company.zoo.aaa.demo;
+
+import com.company.utils.StringUtil;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author lilei
+ * @date 2021-02-07 下午4:34
+ * @apiNote 验证SimpleDateFormat类的线程不安全，使用ThreadLocal解决
+ */
+
+public class Demo33 {
+
+    private static final ThreadLocal<SimpleDateFormat> THREAD_LOCAL = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+    private static ExecutorService service = new ThreadPoolExecutor(50, 100, 1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(100));
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            service.execute(() -> {
+                SimpleDateFormat simpleDateFormat = THREAD_LOCAL.get();
+                String oldDate = simpleDateFormat.format(new Date());
+                try {
+                    Date parseDate = simpleDateFormat.parse(oldDate);
+                    String newDate = simpleDateFormat.format(parseDate);
+                    System.out.println(StringUtil.equals(oldDate, newDate));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                } finally {
+                    THREAD_LOCAL.remove();
+                }
+            });
+
+        }
+    }
+}
