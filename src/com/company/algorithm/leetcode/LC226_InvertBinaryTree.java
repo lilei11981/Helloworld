0812431d275e611a550cf85ecb46a221a2e7dfce
lilei11981 <1198189741@qq.com>
2021-02-19 17:05:29 +0800

    翻转二叉树

--- src/com/company/algorithm/leetcode/LC226_InvertBinaryTree.java
+++ src/com/company/algorithm/leetcode/LC226_InvertBinaryTree.java
@@ -3,7 +3,8 @@ package com.company.algorithm.leetcode;
 /**
  * @author lilei
  * @date 2021-02-18 下午5:53
- * @apiNote 翻转一棵二叉树。
+ * @apiNote 翻转二叉树
+ * 翻转一棵二叉树。
  * <p>
  * 示例：
  * <p>
@@ -28,4 +29,48 @@ package com.company.algorithm.leetcode;
  */
 
 public class LC226_InvertBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)), new TreeNode(7, new TreeNode(6, null, null), new TreeNode(9, null, null)));
+        System.out.println(root);
+        System.out.println(invertTree(root));
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode rootRight = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(rootRight);
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
 }
