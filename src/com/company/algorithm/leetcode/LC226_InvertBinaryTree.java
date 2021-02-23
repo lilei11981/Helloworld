9fef8cc2c73311f5740fa6a2c6172c3f856e54b5
lilei11981 <1198189741@qq.com>
2021-02-23 16:23:28 +0800

    翻转二叉树更新

--- src/com/company/algorithm/leetcode/LC226_InvertBinaryTree.java
+++ src/com/company/algorithm/leetcode/LC226_InvertBinaryTree.java
@@ -14,6 +14,8 @@ package com.company.algorithm.leetcode;
  *   2     7
  *  / \   / \
  * 1   3 6   9
+ * 中序遍历：1 2 3 4 6 7 9
+ *
  * 输出：
  *
  *      4
@@ -21,7 +23,7 @@ package com.company.algorithm.leetcode;
  *   7     2
  *  / \   / \
  * 9   6 3   1
- *
+ * 中序遍历：9 7 6 4 3 2 1
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/invert-binary-tree
@@ -30,19 +32,41 @@ package com.company.algorithm.leetcode;
 
 public class LC226_InvertBinaryTree {
 
+    public static void midOrder(TreeNode treeNode) {
+        if (treeNode != null) {
+            midOrder(treeNode.left);
+            System.out.print(treeNode.val + " ");
+            midOrder(treeNode.right);
+        }
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)), new TreeNode(7, new TreeNode(6, null, null), new TreeNode(9, null, null)));
-        System.out.println(root);
-        System.out.println(invertTree(root));
+        midOrder(root);
+        TreeNode result = invertTree1(root);
+        System.out.println();
+        midOrder(result);
     }
 
     public static TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
         }
-        TreeNode rootRight = root.right;
+        TreeNode rightNode = root.right;
         root.right = invertTree(root.left);
-        root.left = invertTree(rootRight);
+        root.left = invertTree(rightNode);
+        return root;
+    }
+
+    public static TreeNode invertTree1(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        invertTree1(root.left);
+        TreeNode rightNode = root.right;
+        root.right = root.left;
+        root.left = rightNode;
+        invertTree1(root.left);
         return root;
     }
 
