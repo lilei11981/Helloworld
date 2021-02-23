99281c3deee8f39649e12619d103427b7aafaf03
lilei11981 <1198189741@qq.com>
2021-02-23 15:33:34 +0800

    二叉树的中序遍历

---
+++ src/com/company/datastructure/tree/BinaryTreeMidOrder.java
@@ -0,0 +1,60 @@
+package com.company.datastructure.tree;
+
+/**
+ * @author lilei
+ * @date 2021-02-23 下午3:12
+ * @apiNote 二叉树的中序遍历
+ *
+ * 中序遍历：左中右
+ *      4
+ *    /   \
+ *   2     6
+ *  / \   / \
+ * 1   3 5   7
+ *
+ * 中序遍历：1 2 3 4 5 6 7
+ */
+
+public class BinaryTreeMidOrder {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)), new TreeNode(6, new TreeNode(5, null, null), new TreeNode(7, null, null)));
+        midOrder(root);
+    }
+
+    public static void midOrder(TreeNode treeNode) {
+        if (treeNode != null) {
+            midOrder(treeNode.left);
+            System.out.print(treeNode.val + " ");
+            midOrder(treeNode.right);
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+}
