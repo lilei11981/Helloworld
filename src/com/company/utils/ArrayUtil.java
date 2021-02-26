eb3d1815dc6565345e7d0be1bcd8924248d21f91
lilei11981 <1198189741@qq.com>
2021-02-26 17:06:07 +0800

    leetcode岛屿数量

---
+++ src/com/company/utils/ArrayUtil.java
@@ -0,0 +1,27 @@
+package com.company.utils;
+
+/**
+ * @author lilei
+ * @date 2021-02-26 下午4:47
+ * @apiNote
+ */
+
+public class ArrayUtil {
+
+    public static void print(int[][] array) {
+        if (array == null || array.length == 0) {
+            System.out.println("数组为空");
+            return;
+        }
+        int m = array.length;
+        int n = array[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%d ", array[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("----------------------------");
+    }
+}
