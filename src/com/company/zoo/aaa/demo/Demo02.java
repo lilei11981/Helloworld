7e54df614c8140f2d744ad4ba372869d0af0ce1b
lilei11981 <1198189741@qq.com>
2021-03-03 20:44:33 +0800

    提交代码

--- src/com/company/zoo/aaa/demo/Demo02.java
+++ src/com/company/zoo/aaa/demo/Demo02.java
@@ -43,42 +43,165 @@ public class Demo02 {
     // }
 
 
+    // public static void main(String[] args) {
+    //     Node head = new Node();
+    //     System.out.println(head);
+    //     head = head.next;
+    //     System.out.println(head);
+    //     head = head.next;
+    //     System.out.println(head);
+    // }
+    //
+    //
+    // static class Node {
+    //     int val;
+    //     int min;
+    //     Node next;
+    //
+    //     public Node() {
+    //     }
+    //
+    //     public Node(int val, int min) {
+    //         this(val, min, null);
+    //     }
+    //
+    //     public Node(int val, int min, Node next) {
+    //         this.val = val;
+    //         this.min = min;
+    //         this.next = next;
+    //     }
+    //
+    //     @Override
+    //     public String toString() {
+    //         return "Node{" +
+    //                 "val=" + val +
+    //                 ", min=" + min +
+    //                 ", next=" + next +
+    //                 '}';
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     TreeNode root = new TreeNode(1);
+    //     root.left = new TreeNode(2);
+    //     root.right = new TreeNode(2);
+    //     root.left.left = new TreeNode(3);
+    //     root.left.right = new TreeNode(4);
+    //     root.right.left = new TreeNode(4);
+    //     root.right.right = new TreeNode(3);
+    //
+    //     System.out.println(isSymmetric(root));
+    // }
+    //
+    // public static boolean isSymmetric(TreeNode root) {
+    //     if (root == null) {
+    //         return false;
+    //     }
+    //     return check(root.left, root.right);
+    //
+    // }
+    //
+    // private static boolean check(TreeNode left, TreeNode right) {
+    //     if (left == null && right == null) {
+    //         return true;
+    //     }
+    //     if (left == null || right == null || left.val != right.val) {
+    //         return false;
+    //     }
+    //
+    //     return check(left.left, right.right) && check(left.right, right.left);
+    // }
+
+    // private static boolean check(TreeNode left, TreeNode right) {
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.add(left);
+    //     queue.add(right);
+    //
+    //     while (!queue.isEmpty()) {
+    //         TreeNode x = queue.poll();
+    //         TreeNode y = queue.poll();
+    //         if (x == null && y == null) {
+    //             continue;
+    //         }
+    //         if (x == null || y == null || x.val != y.val) {
+    //             return false;
+    //         }
+    //         queue.add(x.left);
+    //         queue.add(y.right);
+    //         queue.add(x.right);
+    //         queue.add(y.left);
+    //     }
+    //     return true;
+    // }
+
+
+    // public static class TreeNode {
+    //     int val;
+    //     TreeNode left;
+    //     TreeNode right;
+    //
+    //     TreeNode() {
+    //     }
+    //
+    //     TreeNode(int val) {
+    //         this.val = val;
+    //     }
+    //
+    //     TreeNode(int val, TreeNode left, TreeNode right) {
+    //         this.val = val;
+    //         this.left = left;
+    //         this.right = right;
+    //     }
+    // }
+
+
+    // public static int maxSubArray(int[] nums) {
+    //     if (nums == null || nums.length == 0) {
+    //         return Integer.MIN_VALUE;
+    //     }
+    //     int result = nums[0];
+    //     int sum = 0;
+    //     for (int num : nums) {
+    //         if (sum > 0) {
+    //             sum += num;
+    //         } else {
+    //             sum = num;
+    //         }
+    //         result = Math.max(sum, result);
+    //     }
+    //     return result;
+    // }
+
     public static void main(String[] args) {
-        Node head = new Node();
-        System.out.println(head);
-        head = head.next;
-        System.out.println(head);
-        head = head.next;
-        System.out.println(head);
+        int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(array));
     }
 
+    private static int maxSubArray(int[] nums) {
+        // int length = nums.length;
+        // int result = nums[0];
+        // for (int i = 0; i < length; i++) {
+        //     int num = nums[i];
+        //     int max = num;
+        //     for (int j = i + 1; j < length; j++) {
+        //         num = nums[j] + num;
+        //         if (num > max) {
+        //             max = num;
+        //         }
+        //     }
+        //     if (result < max) {
+        //         result = max;
+        //     }
+        // }
+        // return result;
 
-    static class Node {
-        int val;
-        int min;
-        Node next;
-
-        public Node() {
+        int result = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            sum = Math.max(sum + num, num);
+            result = Math.max(sum, result);
         }
-
-        public Node(int val, int min) {
-            this(val, min, null);
-        }
-
-        public Node(int val, int min, Node next) {
-            this.val = val;
-            this.min = min;
-            this.next = next;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "val=" + val +
-                    ", min=" + min +
-                    ", next=" + next +
-                    '}';
-        }
+        return result;
     }
 
 }
--- src/com/company/zoo/aaa/demo/Demo10.java
+++ src/com/company/zoo/aaa/demo/Demo10.java
@@ -1,5 +1,8 @@
 package com.company.zoo.aaa.demo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author lilei
  * @date 2021-02-27 下午7:16
@@ -8,20 +11,26 @@ package com.company.zoo.aaa.demo;
 
 public class Demo10 {
 
+    // public static void main(String[] args) {
+    //     Person<Object> person = new Person<>();
+    //     System.out.println(person.getName());
+    // }
+    //
+    // public static class Person<T> {
+    //     T name;
+    //
+    //     public T getName() {
+    //         return name;
+    //     }
+    //
+    //     public void setName(T name) {
+    //         this.name = name;
+    //     }
+    // }
+
     public static void main(String[] args) {
-        Person<Object> person = new Person<>();
-        System.out.println(person.getName());
-    }
-
-    public static class Person<T> {
-        T name;
-
-        public T getName() {
-            return name;
-        }
-
-        public void setName(T name) {
-            this.name = name;
-        }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(null);
+        System.out.println(queue.isEmpty());
     }
 }
