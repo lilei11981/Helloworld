917b4732d382f3b7a48670f66e4bc3c24212e718
lilei <919874206@qq.com>
2021-05-16 12:04:22 +0800

    提交代码

--- src/main/java/com/company/zoo/aaa/demo/Demo16.java
+++ src/main/java/com/company/zoo/aaa/demo/Demo16.java
@@ -1,17 +1,55 @@
 package com.company.zoo.aaa.demo;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author lilei
  * @date 2021-05-15 下午10:32
- * @apiNote
+ * @apiNote 二叉树按层序遍历并收集每一层节点
  */
 
 public class Demo16 {
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        return null;
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        System.out.println(levelOrder(root));
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curResult = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = queue.poll();
+                if (curNode != null) {
+                    curResult.add(curNode.val);
+                    if (curNode.left != null) {
+                        queue.add(curNode.left);
+                    }
+                    if (curNode.right != null) {
+                        queue.add(curNode.right);
+                    }
+                }
+            }
+            result.add(curResult);
+        }
+        return result;
     }
 
     public static class TreeNode {
---
+++ src/main/java/com/company/zoo/aaa/demo/Demo18.java
@@ -0,0 +1,11 @@
+package com.company.zoo.aaa.demo;
+
+/**
+ * @author lilei
+ * @date 2021/5/16 12:03 下午
+ * @apiNote 利用二叉树的先序数组和中序数组重建整颗二叉树
+ */
+
+public class Demo18 {
+
+}
