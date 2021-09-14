b5f863c3ffb96911f1d76e42bc9560030ae72da3
lilei11981 <1198189741@qq.com>
2021-09-14 21:57:58 +0800

    新增Flock类

---
+++ src/main/java/com/company/zoo/aaa/demo04/Flock.java
@@ -0,0 +1,27 @@
+package com.company.zoo.aaa.demo04;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @author lilei
+ * @date 2021-09-14 下午9:54
+ * @apiNote
+ */
+
+public class Flock implements Quackable {
+    ArrayList quackers = new ArrayList();
+
+    public void add(Quackable quacker) {
+        quackers.add(quacker);
+    }
+
+    @Override
+    public void quack() {
+        Iterator iterator = quackers.iterator();
+        while (iterator.hasNext()) {
+            Quackable quacker = (Quackable) iterator.next();
+            quacker.quack();
+        }
+    }
+}
